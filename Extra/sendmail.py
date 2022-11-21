6436e3da3fcb131024c68159232eb3025f5a702e
Soumya Panja <panjasoumya22@gmail.com>
2022-11-21 23:16:10 +0530

    Changed content of sendmail in extras, Updated webdevelopment files names, Added new data analysis

--- Extra/sendmail.py
+++ Extra/sendmail.py
@@ -3,4 +3,4 @@ password = input(":::")
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login("testpanjasoumya22@gmail.com", password)
-    server.sendmail("testpanjasoumya22gmail.com", "sangbarta2000@gmail.com", "Ai khani cheele")
+    server.sendmail("testpanjasoumya22gmail.com", "HELLO@gmail.com", "tHIS IS a python generated mail.")
