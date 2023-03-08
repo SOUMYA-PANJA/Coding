a180cd9486080317056ed23cc55abaa9dd24a689
Soumya Panja <panjasoumya22@gmail.com>
2023-03-08 12:34:39 +0530

    Restructured the System Design and Data Science Folders

---
+++ System_Design/Assignments/Codes/17_Constructor_10th_Feb_2023/Question8.java
@@ -0,0 +1,10 @@
+class Student{
+
+}
+
+public class Question8 {
+    public static void main(String[] args) {
+        Student obj = new Student();
+        System.out.println("Object created...");
+    }
+}
---
+++ System_Design/Codes/13_Abstraction_Abstract_And_Final_Keyword/Program1AbstractKeyword.java
@@ -0,0 +1,49 @@
+// Class having atleast one abstract member must be declared abstract.
+// If all the members in a class are abstract then the class achieved full abstraction
+// There can't abstract variable and constructor(it have first line as super(), which have a body.).
+// We can create references of abstract class, so that is can provide upcasting and dynamic polymorphism.. But we can instantiate an abstract class.
+abstract class Plane{
+    abstract public void fly();
+    abstract public void takeOff();
+    public void landing(){
+        System.out.println("Plane is landing.");
+    }
+}
+
+class AeroPlane extends Plane{
+    public void fly() {
+        System.out.println("Aeroplane is flying at medium height.");
+    }
+    public void takeOff() {
+        System.out.println("Aeroplane is taking off.");
+    }
+    
+}
+
+class CargoPlane extends Plane{
+    public void fly() {
+        System.out.println("Cargoplane is flying at larger height");
+    }
+    public void takeOff() {
+        System.out.println("Cargoplane is taking offf.");
+    }
+    
+}
+
+class AirPort{
+    public void polyMorphism(Plane ref){
+        // ref is a reference of abstract class
+        ref.fly();
+        ref.takeOff();
+        ref.landing();
+        System.out.println("-------------------------------------");
+    }
+}
+
+public class Program1AbstractKeyword {
+    public static void main(String[] args) {
+        AirPort obj = new AirPort();
+        obj.polyMorphism(new AeroPlane());
+        obj.polyMorphism(new CargoPlane());
+    }
+}
